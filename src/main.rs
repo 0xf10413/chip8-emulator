495f08dc16ae0c3094be79a59ca8b1ee62ade571
0xf10413 <flo.g@home.local>
2024-10-20 03:12:36 +0200

    bugfix: fix key mapping

--- src/main.rs
+++ src/main.rs
@@ -63,16 +63,68 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
-                Event::KeyDown { keycode: Some(Keycode::NUM_8), ..} => emulator.keys_pressed[0x00] = true,
-                Event::KeyUp { keycode: Some(Keycode::NUM_8), ..} => emulator.keys_pressed[0x00] = false,
+                Event::KeyDown { keycode: Some(Keycode::KP_0), ..} => emulator.keys_pressed[0x00] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_0), ..} => emulator.keys_pressed[0x00] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::KP_1), ..} => emulator.keys_pressed[0x01] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_1), ..} => emulator.keys_pressed[0x01] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::KP_2), ..} => emulator.keys_pressed[0x02] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_2), ..} => emulator.keys_pressed[0x02] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::KP_3), ..} => emulator.keys_pressed[0x03] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_3), ..} => emulator.keys_pressed[0x03] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::KP_4), ..} => emulator.keys_pressed[0x04] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_4), ..} => emulator.keys_pressed[0x04] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::KP_5), ..} => emulator.keys_pressed[0x05] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_5), ..} => emulator.keys_pressed[0x05] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::KP_6), ..} => emulator.keys_pressed[0x06] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_6), ..} => emulator.keys_pressed[0x06] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::KP_7), ..} => emulator.keys_pressed[0x07] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_7), ..} => emulator.keys_pressed[0x07] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::KP_8), ..} => emulator.keys_pressed[0x08] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_8), ..} => emulator.keys_pressed[0x08] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::KP_9), ..} => emulator.keys_pressed[0x09] = true,
+                Event::KeyUp { keycode: Some(Keycode::KP_9), ..} => emulator.keys_pressed[0x09] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::A), ..} => emulator.keys_pressed[0xA] = true,
+                Event::KeyUp { keycode: Some(Keycode::A), ..} => emulator.keys_pressed[0xA] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::Z), ..} => emulator.keys_pressed[0xB] = true,
+                Event::KeyUp { keycode: Some(Keycode::Z), ..} => emulator.keys_pressed[0xB] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::E), ..} => emulator.keys_pressed[0xC] = true,
+                Event::KeyUp { keycode: Some(Keycode::E), ..} => emulator.keys_pressed[0xC] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::R), ..} => emulator.keys_pressed[0xD] = true,
+                Event::KeyUp { keycode: Some(Keycode::R), ..} => emulator.keys_pressed[0xD] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::T), ..} => emulator.keys_pressed[0xE] = true,
+                Event::KeyUp { keycode: Some(Keycode::T), ..} => emulator.keys_pressed[0xE] = false,
+
+                Event::KeyDown { keycode: Some(Keycode::Y), ..} => emulator.keys_pressed[0xF] = true,
+                Event::KeyUp { keycode: Some(Keycode::Y), ..} => emulator.keys_pressed[0xF] = false,
+
                 _ => {}
             }
         }
 
         // Several cpu cycles per render cycle
-        for _ in 0..4 {
+        for _ in 0..20 {
             emulator.process_next_instruction();
             println!();
+        }
+
+        println!("{:?}", emulator.keys_pressed);
+
+        if emulator.system_clock > 0 {
+            emulator.system_clock -= 1;
         }
 
         canvas.set_draw_color(Color::WHITE);
